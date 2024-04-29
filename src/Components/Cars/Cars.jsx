84aef217843fd601a1704a07684e443f214936a4
Maryammmy <malaksameh18112004@gmail.com>
2024-04-29 13:18:41 +0300

    k

--- src/Components/Cars/Cars.jsx
+++ src/Components/Cars/Cars.jsx
@@ -87,7 +87,7 @@ const Cars = () => {
                   </div>
                 </div>
                 <div className="col-md-7 padding-right pe-xl-4 pe-xxl-0" onClick={() => handleTitleClick(article)}>
-                  <h4 className='fw-bolder title '>{article.title}</h4>
+                  <h4 className='fw-bolder title pt-sm-2 pt-md-0'>{article.title}</h4>
                   <p className='time'>{formatDate(article.date)}</p>
                 </div>
                 <div className='pt-2'>
